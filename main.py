ebba262a716d630ef49c9536f1604571f055daa2
parhamztlo-ship-it <parhamztlo@gmail.com>
2026-07-27 15:02:35 +0330

    fix token

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import telebot
 from telebot import types
 
 # ====== توکن‌ها ======
-TELEGRAM_TOKEN = "8799705703:AAG3UUjtHcnTXty-Hn8iXfT8jkhVWa137ck"
+TELEGRAM_TOKEN = "8799705703:AAEYxgQbQWK-Fr1QHDxY7aJ8ys1YLSKsm_A"
 AUDD_API_TOKEN = "اینجا_توکن_audd_رو_بذار"
 # ===================
 
